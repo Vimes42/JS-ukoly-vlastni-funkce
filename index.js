921d637aff827c2ea3b2302abb36b3c37447e2a7
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-08 23:15:54 +0100

    splnene ukoly

---
+++ index.js
@@ -0,0 +1,40 @@
+//úkol č. 1
+// tohle by i podle řešení mělo fungovat, ale nemůžu přijít na to, proč se mi to nevypisuje do konzole.
+
+const parseDate = (date) => {
+    return {
+        day: Number(date.slice(0, 2)),
+        month: Number(date.slice(3, 5)),
+        year: Number(date.slice(6)),
+    }
+}
+
+//úkol č. 2
+let resultDate = {day: 2, month: 12, year: 2022}
+
+const formatDate = (datum) => {
+    
+    let [den, mesic, rok] = [datum.day.toString(), datum.month.toString(), datum.year.toString()]
+
+    return den.padStart(2, "0") + "." + mesic.padStart(2, "0") + "." + rok
+    
+}
+
+//úkol č.3
+const round = (cislo) => {
+    const celeCislo = Math.trunc(cislo)
+    const desetinne = cislo - celeCislo
+    
+    if (desetinne === 0.5) {
+        if (celeCislo % 2 === 0) {
+            return celeCislo
+        } else {
+            return celeCislo + 2 * desetinne
+        }  
+    } 
+        return Math.round(cislo)
+}
+
+
+
+
